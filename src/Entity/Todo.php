b9f76aa0ffed180c01481ead2ca045bdd08cb8fe
coltrida <coltrida@gmail.com>
2020-12-20 20:17:30 +0100

    inserita validazione sul nome con messaggio standard

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\TodoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TodoRepository::class)
@@ -18,6 +19,7 @@ class Todo
     private $id;
 
     /**
+     * @Assert\Email()
      * @ORM\Column(type="string", length=255)
      */
     private $name;
